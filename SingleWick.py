89f8fdf3e5eac9d367fcefb51da327190f277bf6
oldJonny <scu_lyz@163.com>
2017-05-11 09:42:25 +0800

    四个参数，前三个参数的分母都是前一天的最低价 low[i]

---
+++ SingleWick.py
@@ -0,0 +1,104 @@
+import os
+import pprint, pickle
+import pandas as pd
+from sklearn.cluster import KMeans
+import datetime
+
+
+class SingleWick():
+    def __init__(self, data):
+        # data是该股票的全部数据
+        self.data = data
+        # open：开盘价
+        self.open = data['open']
+        # close：收盘价
+        self.close = data['close']
+        # high：最高价
+        self.high = data['high']
+        # low最低价
+        self.low = data['low']
+        # 文件路径
+        # self.dirpath = dirpath
+
+    # 中影线
+    def get_middlewick_rate(self):
+
+        middlewick = []
+
+        for i in range(len(self.open) - 1):
+            # 上涨
+            if self.close[i + 1] > self.open[i + 1]:
+                para1 = (self.close[i + 1] - self.open[i + 1]) / self.low[i]
+            # 下跌
+            else:
+                para1 = (self.open[i + 1] - self.close[i + 1]) / self.low[i]
+            middlewick.append(para1)
+        return middlewick
+
+    # 上影线
+    def get_upperwick_rate(self):
+        upperwick = []
+        for i in range(len(self.open) - 1):
+            # 上涨
+            if self.close[i + 1] > self.open[i + 1]:
+                para2 = (self.high[i + 1] - self.close[i + 1]) / self.low[i]
+                upperwick.append(para2)
+            # 下跌
+            else:
+                para2 = (self.high[i + 1] - self.open[i + 1]) / self.low[i]
+            upperwick.append(para2)
+        return upperwick
+
+    # 下影线
+    def get_lowerwick_rate(self):
+        lowerwick = []
+        for i in range(len(self.open) - 1):
+            # 上涨
+            if self.close[i + 1] > self.open[i + 1]:
+                para3 = (self.open[i + 1] - self.low[i + 1]) / self.low[i]
+                lowerwick.append(para3)
+            # 下跌
+            else:
+                para3 = (self.close[i + 1] - self.low[i + 1]) / self.low[i]
+            lowerwick.append(para3)
+        return lowerwick
+
+    # 当天涨跌幅，因为变量close_dif自带正负，所以变量的正负代表涨跌，变量的值代表涨跌幅
+    def get_close_dif_rate(self):
+        close_dif = []
+        for i in range(len(self.open) - 1):
+            para4 = (self.close[i + 1] - self.close[i]) / self.close[i]
+            close_dif.append(para4)
+        return close_dif
+
+    def get_singlewick(self):
+
+        middlewick = self.get_middlewick_rate()
+        upperwick = self.get_upperwick_rate()
+        lowerwick = self.get_lowerwick_rate()
+        close_dif = self.get_close_dif_rate()
+        singlewick = list(zip(middlewick, upperwick, lowerwick, close_dif))
+        for i in singlewick:
+            if i[3] >= 0.11 or i[3] <= -0.11:
+                singlewick.remove(i)
+        return singlewick
+
+
+
+
+if __name__ == '__main__':
+    global X
+    X = []
+    dirpath = r'C:\Users\lyzdsb\Desktop\finalPrograme'
+    code_list = os.listdir(dirpath)
+    for code in code_list:
+        file_path = 'C:/Users/lyzdsb/Desktop/finalPrograme/' + code
+        pkl_file = open(file_path, 'rb')
+        data = pickle.load(pkl_file)
+        pkl_file.close()
+        a = SingleWick(data)
+        temp = a.get_singlewick()
+        X = X + temp
+    output = open(dirpath + 'raw_data_version2.pkl', 'wb')
+    pickle.dump(X, output, -1)
+    output.close()
