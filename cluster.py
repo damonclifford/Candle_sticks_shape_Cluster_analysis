4c648aeb5aab245fe5a89d915e52b060f26dd668
oldJonny <scu_lyz@163.com>
2017-10-04 21:43:44 +0800

    train and save models with different number of clusters

--- cluster.py
+++ cluster.py
@@ -2,36 +2,30 @@ from sklearn.cluster import KMeans
 from sklearn.externals import joblib
 import pickle
 
-file_path = r'C:\Users\lyzdsb\PycharmProjects\untitled3\data\raw_data_integrate' + r'\raw_data_para3.pkl'
-mode_path = r'C:\Users\lyzdsb\PycharmProjects\untitled3\data\para3_data'
+file_path = r'C:\Users\lyzdsb\PycharmProjects\finalProject\data\raw_data_integrate' + r'\raw_data_para3.pkl'
+mode_path = r'C:\Users\lyzdsb\PycharmProjects\finalProject\data\para3_data'
 pkl_file = open(file_path, 'rb')
 X = pickle.load(pkl_file)
 
-# 训练模型并且保存
+# train and save models with different number of clusters
 for i in range(10, 51, 2):
     clf = KMeans(n_clusters=i)
     s = clf.fit(X)
-    # 需要设置参数 compress = 3， 否则保存的模型文件的同时会生成很多杂乱的文件
+    # compress = 3
     joblib.dump(clf, mode_path + r'\mode_i_' + str(i) + '.pkl', compress=3)
     print(i, clf.inertia_)
 
-# for i in range(20, 24):
-#     clf = joblib.load('mode_i_' + str(i) + '.pkl')
-#     print(clf.cluster_centers_)
+# overall_distance = inner_distance + inter_distance
 
-# clf = joblib.load('mode_i_' + str(i) + '.pkl')
+# load models
+joblib.dump(clf, 'mode_i_25.pkl')
+clf = joblib.load('mode_i_25.pkl')
+centerCollection = clf.cluster_centers_
 
-# 模型的保存和调用
-# joblib.dump(clf, 'mode_i_25.pkl')
-# clf = joblib.load('mode_i_35.pkl')
-# centerCollection = clf.cluster_centers_
-# print(clf.cluster_centers_)
-# global result
-# result = 0
-# for i in range(len(centerCollection)):
-#     for j in range(i + 1, len(centerCollection)):
-#         dist = numpy.linalg.norm(centerCollection[i] - centerCollection[j]).item()
-#         result = dist + result
-# print(result)
-# 簇内距离和
-# print(clf.inertia_)
+global result
+result = 0
+for i in range(len(centerCollection)):
+    for j in range(i + 1, len(centerCollection)):
+        dist = numpy.linalg.norm(centerCollection[i] - centerCollection[j]).item()
+        result = dist + result
+
