e2c1d4dd467a733afb7220a863e8690b8d48f94d
oldJonny <scu_lyz@163.com>
2017-10-04 22:38:45 +0800

    identified the best model
    identified the best model by metric and ploted the results

--- plot_K.py
+++ plot_K.py
@@ -2,32 +2,34 @@ import matplotlib.pyplot as plt
 from sklearn.externals import joblib
 import numpy as np
 
-modePath = r'C:\Users\lyzdsb\PycharmProjects\untitled3\data\para3_data'
-dataPath = r'C:\Users\lyzdsb\PycharmProjects\untitled3\data\raw_data_integrate'
-# 第一种评价方法
+modePath = r'C:\Users\lyzdsb\PycharmProjects\finalProject\data\para3_data'
+dataPath = r'C:\Users\lyzdsb\PycharmProjects\finalProject\data\raw_data_integrate'
+# calculate the metric 
+# the formula of metric: ratio = inner_distance / (inner_distance + inter_distance)
+# range[0,1], greater ratio means better performance
 X = []
 Y = []
 dataSet = []
 for i in range(10, 51, 2):
     clf = joblib.load(modePath + r'\mode_i_' + str(i) + '.pkl')
     centerCollection = clf.cluster_centers_
-    between = 0
-    within = clf.inertia_
+    inner_distance = 0
+    inter_distance = clf.inertia_
     for j in range(len(centerCollection)):
         for k in range(j + 1, len(centerCollection)):
             dist = np.linalg.norm(centerCollection[j] - centerCollection[k])
-            between = dist + between*1.018
+            inter_distance = inter_distance + inner_distance*1.018
 # para = 1.018
-    ratio = between / (between + within)
+    ratio = inner_distance / (inner_distance + inter_distance)
     X.append(i)
     Y.append(ratio)
     dataSet.append((i, ratio))
 
-# 绘图
-plt.figure(figsize=(8, 4))  # 创建绘图对象
-plt.plot(X, Y, "b--", linewidth=1)  # 在当前绘图对象绘图（X轴，Y轴，蓝色虚线，线宽度）
-plt.xlabel("K")  # X轴标签
-plt.ylabel("Sum of Squares for Error(ratio)")  # Y轴标签
-plt.title("Sum of Squares for Error versus k(ratio)")  # 图标题
+# plot the metric of different metrics
+plt.figure(figsize=(8, 4)) 
+plt.plot(X, Y, "b--", linewidth=1)  
+plt.xlabel("K") 
+plt.ylabel("Sum of Squares for Error(ratio)") 
+plt.title("Sum of Squares for Error versus k(ratio)")  
 plt.grid(True)
-plt.show()  # 显示图
+plt.show()  
